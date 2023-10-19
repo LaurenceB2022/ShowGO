1d2f36b69fc0b9af6a9ee32c7d754f60746c44e8
NATHANIEL ENDICK <nathaniel.endick@gmail.com>
2023-10-19 16:17:47 -0400

    Backend Project Setup (Connected to DB)

---
+++ src/test/java/com/shifthappens/showgo/ShowgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shifthappens.showgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
