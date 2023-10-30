561f9f1c653f9f5a53f2829e247a6c1039e6ab3f
NATHANIEL ENDICK <nathaniel.endick@gmail.com>
2023-10-30 18:30:48 -0400

    Search bar HTML/CSS + removing some code warnings

--- src/Components/GridEvents.jsx
+++ src/Components/GridEvents.jsx
@@ -1,7 +1,5 @@
 import 'index.css';
 import styles from 'Components/GridEvents.module.css';
-import {Link} from 'react-router-dom'
-import React, {useState, useEffect} from 'react';
 import Event from 'Components/Event';
 import ShowGoLogo from 'Assets/ShowGoLogo.png';
 
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,27 +1,51 @@
 import 'index.css';
 import styles from 'Components/SearchBar.module.css'
 import Search from 'Assets/Search.svg';
+import { useState } from 'react';
 
 export default function SearchBar (props) {
-    const enterKeyEvent = new KeyboardEvent('keydown', {
-        key: 'Enter',
-        code: 'Enter',
-        which: 13,
-        keyCode: 13,
-      });
+
+    const [searchType, setSearchType] = useState('title'); //options: title, date-range, cost
 
     function search(event) {
-        if(event.type != 'click' && event.key != 'Enter') return;
+        if(event.type !== 'click' && event.key !== 'Enter') return;
         // TODO
         // searching functionalities
     }
 
     return (
         <div id={props.id}>
-            <div id={styles.search}>
-                <input id={styles.input} type="text" placeholder='Search' onKeyUp={search}/>
-                <img id={styles.img} src={Search} onClick={search} alt=""/>
-            </div> 
-        </div>      
+            <div className={styles.searchfields}>
+                <div id={styles.search}>
+                    <input id={styles.input} type="text" placeholder='Search' onKeyUp={search}/>
+                    <img id={styles.img} src={Search} onClick={search} alt=""/>
+                </div>
+                <select onChange={() => setSearchType(document.getElementById(styles.searchType).value)} id={styles.searchType}>
+                    <option value="title">Title</option>
+                    <option value="date-range">Date Range</option>
+                    <option value="cost">Cost</option>
+                </select>
+            </div>
+            {searchType === 'date-range' &&
+                <>
+                    {/* TO DO -> text validation / formatting */}
+                    <label class={styles.searchLabel}>From</label>
+                    <input class={styles.searchInput} type="text" placeholder='yyyy-mm-dd' onKeyUp={search}/>
+                    <br></br>
+                    <label class={styles.searchLabel}>To</label>
+                    <input class={styles.searchInput}type="text" placeholder='yyyy-mm-dd' onKeyUp={search}/>
+                </>
+            }
+            {searchType === 'cost' &&
+                <>
+                    {/* TO DO -> text validation / formatting */}
+                    <label class={styles.searchLabel}>Min</label>
+                    <input class={styles.searchInput} type="text" placeholder='Minimum cost' onKeyUp={search}/>
+                    <br></br>
+                    <label class={styles.searchLabel}>Max</label>
+                    <input class={styles.searchInput}type="text" placeholder='Maximum cost' onKeyUp={search}/>
+                </>
+            }
+        </div>   
     );
 }
