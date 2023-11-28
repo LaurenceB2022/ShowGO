bf8659f49b6674354ae8fb2496cbff9a1b780821
Tyler Harper <tjharper2020@gmail.com>
2023-11-27 21:43:41 -0500

    T013 and T012 tests

---
+++ src/main/java/com/shifthappens/showgo/exceptions/InvalidEventCreationAdvice.java
@@ -0,0 +1,15 @@
+package com.shifthappens.showgo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class InvalidEventCreationAdvice {
+    @ResponseBody
+  @ExceptionHandler(InvalidEventCreationException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  String employeeNotFoundHandler(InvalidEventCreationException ex) {
+    return ex.getMessage();
+  }
+}
