67b792629fd725faf92d76aa41f18cde60f30cca
tankman72 <jwl102@scarletmail.rutgers.edu>
2023-11-30 14:27:17 -0500

    Commenting on BlockedUser

--- src/main/java/com/shifthappens/showgo/entities/BlockedUser.java
+++ src/main/java/com/shifthappens/showgo/entities/BlockedUser.java
@@ -8,27 +8,30 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
-
+/*
+ * The BlockedUser class is the class that creates a blocked user object and stores that 
+ * object in our database 
+ */
 @Entity
 @Table(name = "blocked_users")
 @Data
 public class BlockedUser {
-    
+    //Generating the guid string
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String guid;
-
+    //joining the table that holds the blocked users 
     @ManyToOne
     @JoinColumn(name= "user_username")
     private User user;
-
+    //joining the table that holds the blocked users 
     @ManyToOne
     @JoinColumn(name= "venue_username")
     private Venue venue;
-
+    //Empty method call constructor 
     public BlockedUser() {
     }
-
+    //Constuctor that takes a user and venue argument
     public BlockedUser(User user, Venue venue) {
         this.user = user;
         this.venue = venue;
--- src/main/java/com/shifthappens/showgo/repositories/BlockedUserRepository.java
+++ src/main/java/com/shifthappens/showgo/repositories/BlockedUserRepository.java
@@ -10,22 +10,24 @@ import com.shifthappens.showgo.entities.User;
 import com.shifthappens.showgo.entities.Venue;
 
 import java.util.List;
-
+/*
+ * This BlockedUserRepository file is the repository that maps our database requests for our project
+ */
 
 @Repository
 public interface BlockedUserRepository extends CrudRepository<BlockedUser, String>{
     List<BlockedUser> findByVenue(Venue venue);
 
     List<BlockedUser> findByUser(User user);
-
+    //This Query gets the blocked users from the database where the user username and venue user name match a certain search criteria    
     @Query(value = "SELECT * FROM blocked_users u WHERE u.user_username = :user and u.venue_username = :venue",
             nativeQuery = true)
     BlockedUser findByUserAndVenue(@Param("user") String userUsername, @Param("venue") String venueUsername);
-
+    //This Query gets the blocked users from the database where the user username match a certain search criteria    
     @Query(value = "SELECT * FROM blocked_users u WHERE u.user_username = :user",
             nativeQuery = true)
     List<BlockedUser> findByUserUsername(@Param("user") String userUsername);
-
+    //This Query gets the blocked users from the database where the venue username match a certain search criteria    
     @Query(value = "SELECT * FROM blocked_users u WHERE u.venue_username = :venue",
             nativeQuery = true)
     List<BlockedUser> findByVenueUsername(@Param("venue") String venueUsername);
